77add8ac521a5062fcdca33eadf60bda7ffe313a
Pedro Nadalon Klippel <pnklippel@gmail.com>
2026-08-06 23:21:55 -0300

    arumando a raridade

--- statics/js/inventario/inventario_data.js
+++ statics/js/inventario/inventario_data.js
@@ -40,7 +40,7 @@ const OPTIONS_RARIDADE = [
     { v: "incomum", t: "Incomum" },
     { v: "raro", t: "Raro" },
     { v: "epico", t: "Épico" },
-    { v: "mitico", t: "Mítico" },
+    { v: "mitico", t: "#00ffff" },
     { v: "especial", t: "Relíquia" }
 ];
 
